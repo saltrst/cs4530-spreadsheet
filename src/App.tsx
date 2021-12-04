d9896ce8d89a1e981275cb051bd9e68213f9d2ed
Will Kofski <will.kofski@gmail.com>
2021-12-04 11:56:11 -0500

    removed comments from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import './App.css';
-// import { MenuBar } from './components/MenuBar';
-// import { FormulaInput } from './components/FormulaInput';
-// import Cell from './components/Cell';
 import Spreadsheet from './components/Spreadsheet';
 
 function App() {
-  // const [input, setFormulaInput] = useState('');
-  // const [value, setCellValue] = useState({});
-
-  // function updateInput(input: string) {
-  //   setFormulaInput(input);
-  // }
-
-  // useEffect(() => {
-  //   const cellValue = input;
-  //   if (cellValue) {
-  //     setCellValue(cellValue);
-  //   }
-  // }, [input]);
-
   return (
     <div className='App'>
-      {/* <MenuBar></MenuBar> */}
-      {/* <FormulaInput updateInput={updateInput}></FormulaInput> */}
       <Spreadsheet x={6} y={20}></Spreadsheet>
     </div>
   );
